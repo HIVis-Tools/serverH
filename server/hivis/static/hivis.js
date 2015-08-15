7c5eab0f40ad7b08cdf5821d04caaeabd5f3f445
Jon <jambler24@gmail.com>
2015-08-16 00:36:22 +0200

    Now with added crouching other hidden Additional_form

--- server/hivis/static/hivis.js
+++ server/hivis/static/hivis.js
@@ -1,11 +1,17 @@
-// A $( document ).ready() block.
-$( document ).ready(function() {
-        $("#id_protein_choice").change(function(){
+$(document).ready(function(){
+	$("#id_ref_string").hide();
+
+    $('#id_protein_choice').on('change', function() {
+	if ( this.value == 'other')
+      {
+        $("#id_ref_string").show();
+      }
+      else
+      {
         $("#id_ref_string").hide();
-        });
-	$("#id_protein_choice").val("other").change(function(){
-	$("#id_ref_string").show();	
-	});
+      }
+    });
 });
+
 
 
