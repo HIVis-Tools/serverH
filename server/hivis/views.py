1dfac2ea257d268ae4e5730194a9c81e5c727935
Jon <jambler24@gmail.com>
2015-08-16 15:21:38 +0200

    File upload done and working, hazaa!

--- server/hivis/views.py
+++ server/hivis/views.py
@@ -21,55 +21,68 @@ def alignment(request):
     return render(request, 'hivis/alignment.html', context)
 
 def test_alignment(request):
-	# Input formatting
-	fasta_string = request.POST.get('fasta_string', False)
-	user_name = request.POST.get('user_name', False)
-	protein_choice = request.POST.get('protein_choice', False)
-	file_up = UploadFileForm(request.POST, request.FILES)
-	file_obj = file_up.save()
 
-	user_name = user_name.replace(' ','_')
-	job_id = user_name + str(uuid.uuid4())[-6:]
+	job_id = str(uuid.uuid4())[-6:]
 
+	if len(request.POST.get('fasta_string', False)) > 0:
+		# Input formatting
+		fasta_string = request.POST.get('fasta_string', False)
+		protein_choice = request.POST.get('protein_choice', False)
+	
+
+		# Selecting the ref seq
+		if protein_choice != 'other':
+			ref_seq_path = './hivis/static/HXB2_ref_seq/' + protein_choice + '.fasta'
+			ref_seq_file = open(ref_seq_path, 'r')
+			ref_seq = ref_seq_file.read()
+	
+		else:
+			ref_seq = request.POST.get('ref_string', False)
+	
+		# Variable setting
+		temp_files = './hivis/static/temp/' + job_id + '/'
+		os.system('mkdir ' + temp_files)
+    	    
+		if _platform == "linux" or _platform == "linux2":
+			muscle_path = './hivis/programs/muscle3.8.31_i86linux32'
+		elif _platform == "darwin":
+			muscle_path = './hivis/programs/muscle3.8.31_i86darwin64'
+		elif _platform == "win32":
+			muscle_path = './hivis/programs/muscle3.8.31_i86win32.exe'
+	
+		# Write to file
+		fasta_output = open(temp_files + job_id + '.fa', 'w')
+		fasta_string = ref_seq + '\n' + fasta_string
+		fasta_output.write(fasta_string)
+		fasta_output.close()
+		e = muscle_align(job_id, muscle_path, temp_files)
+		
+		temp_dict = aln_to_dict(temp_files + job_id)
+	
+
+	#-------------------------------------- file upload --------------------------------------
 	# File upload magic
-	uploaded_filepath = './hivis/static/temp/uploads/' + job_id + '.aln'
-	export_file = open(uploaded_filepath, 'w')
 	
-	for line in request.FILES['filebutton']:
-		export_file.write(line)
-	export_file.close()
-	# End file upload bit
-	
+	#file_up = UploadFileForm(request.POST, request.FILES)
+	#file_obj = file_up.save()
 
-	# Selecting the ref seq
-	if protein_choice != 'other':
-		ref_seq_path = './hivis/static/HXB2_ref_seq/' + protein_choice + '.fasta'
-		ref_seq_file = open(ref_seq_path, 'r')
-		ref_seq = ref_seq_file.read()
-	
 	else:
-		ref_seq = request.POST.get('ref_string', False)
+		
+		temp_files = './hivis/static/temp/' + job_id + '/'
+		os.system('mkdir ' + temp_files)
+
+		uploaded_filepath = temp_files + job_id + '.aln'
+		export_file = open(uploaded_filepath, 'w')
+		
+		for line in request.FILES['filebutton']:
+			export_file.write(line)
+		export_file.close()
+
+		temp_dict = aln_to_dict('/Volumes/HDD/Projects/serverH/server/hivis/static/temp/uploads/769bfb')
+		# End file upload bit
+
 	
-	# Variable setting
-	temp_files = './hivis/static/temp/' + job_id + '/'
-        os.system('mkdir ' + temp_files)
-        
-        if _platform == "linux" or _platform == "linux2":
-            muscle_path = './hivis/programs/muscle3.8.31_i86linux32'
-        elif _platform == "darwin":
-            muscle_path = './hivis/programs/muscle3.8.31_i86darwin64'
-        elif _platform == "win32":
-            muscle_path = './hivis/programs/muscle3.8.31_i86win32.exe'
-	
-	# Write to file
-	fasta_output = open(temp_files + job_id + '.fa', 'w')
-	fasta_string = ref_seq + '\n' + fasta_string
-	fasta_output.write(fasta_string)
-	fasta_output.close()
-	e = muscle_align(job_id, muscle_path, temp_files)
-	
-	temp_dict = aln_to_dict(temp_files + job_id)
-	
+	# Come together here to pass to js
 	out_put_test = dict_to_json(temp_dict)
 	save_json(dict_to_json(temp_dict), temp_files, job_id)
 	#return HttpResponse(job_id)
